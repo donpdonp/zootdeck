dd362d0cbb9e9334618f9313b15928f63491285a
Don Park <donp@donp.org>
2019-07-02 14:24:46 -0700

    lmdb go!

--- src/db/lmdb.zig
+++ src/db/lmdb.zig
@@ -36,26 +36,44 @@ pub fn stats() void {
 
 pub fn write(namespace: []const u8, key: []const u8, value: []const u8, allocator: *Allocator) !void {
   var txnptr = allocator.create(*c.struct_MDB_txn) catch unreachable;
-  var ctxnMaybe: [*c]?*c.struct_MDB_txn = @ptrCast([*c]?*c.struct_MDB_txn, txnptr);
+  var ctxnMaybe = @ptrCast([*c]?*c.struct_MDB_txn, txnptr);
   var ret = c.mdb_txn_begin(env, null, 0, ctxnMaybe);
-  if (ret != 0) {
-    return error.lmdb;
-  }
-  warn("lmdb write {} {}={}\n", namespace, key, value);
-  var dbiptr = allocator.create(c.MDB_dbi) catch unreachable;
-  ret = c.mdb_dbi_open(txnptr.*, util.sliceToCstr(allocator, namespace), c.MDB_CREATE, dbiptr.*);
-  var keyptr = @intToPtr(?*c_void, @ptrToInt(key.ptr));
-  var mdbValKey = c.MDB_val{.mv_size = key.len, .mv_data = keyptr};
-  var valueptr = @intToPtr(?*c_void, @ptrToInt(value.ptr));
-  var mdbValVal = c.MDB_val{.mv_size = value.len, .mv_data = valueptr};
-  ret = c.mdb_put(txnptr.*, dbiptr.*, &mdbValKey, &mdbValVal, 0);
-  ret = c.mdb_txn_commit(txnptr.*);
-  if (ret != 0) {
-    return error.lmdb;
+  if (ret == 0) {
+    warn("lmdb write {} {}={}\n", namespace, key, value);
+    var dbiptr = allocator.create(c.MDB_dbi) catch unreachable;
+    ret = c.mdb_dbi_open(txnptr.*, null, c.MDB_CREATE, dbiptr);
+    if (ret == 0) {
+      // TODO: seperator issue. perhaps 2 byte invalid utf8 sequence
+      var fullkey = std.fmt.allocPrint(allocator, "{}:{}", namespace, key) catch unreachable;
+      var mdb_key = mdbVal(fullkey, allocator);
+      var mdb_value = mdbVal(value, allocator);
+      ret = c.mdb_put(txnptr.*, dbiptr.*, mdb_key, mdb_value, 0);
+      if (ret == 0) {
+        ret = c.mdb_txn_commit(txnptr.*);
+        if (ret == 0) {
+          warn("mdb txn COMMIT!\n");
+          _ = c.mdb_dbi_close(env, dbiptr.*);
+        } else {
+          return error.mdb_txn_commit;
+        }
+      } else {
+        warn("mdb_put ERR {}\n", ret);
+        return error.mdb_put;
+      }
+    } else {
+      warn("mdb_dbi_open ERR {}\n", ret);
+      return error.mdb_dbi_open;
+    }
   } else {
-    warn("mdb txn COMMIT!\n");
-    _ = c.mdb_dbi_close(env, dbiptr.*);
+    warn("mdb_txn_begin ERR {}\n", ret);
+    return error.mdb_txn_begin;
   }
-  var zigfool=false;
-  if(zigfool) return error.shutup;
+}
+
+fn mdbVal(data: []const u8, allocator: *Allocator) *c.MDB_val {
+  var dataptr = @intToPtr(?*c_void, @ptrToInt(data.ptr));
+  var mdb_val = allocator.create(c.MDB_val) catch unreachable;
+  mdb_val.mv_size = data.len;
+  mdb_val.mv_data = dataptr;
+  return mdb_val;
 }
