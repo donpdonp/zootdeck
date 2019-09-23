25b5850830a1f0dbb263ae4124b0cf42d4b92df7
Andrew Kelley <andrew@ziglang.org>
2019-09-23 15:27:57 -0400

    update to work with zig master branch
    I added pkg-config integration to Zig master branch to make your build
    script simpler. This allowed me to remove most of the hard-coded paths
    from build.zig.
    
    I also took advantage of `b.standardTargetOptions` and `b.setTheTarget`.
    This means that `zig build` will create a native executable - which will
    look in standard system directories for libraries - and
    `zig build -Dtarget=x86_64-linux-gnu` will create an executable suitable
    for distributing.
    
    I removed `exe.setOutputDir` and added `exe.install()`. This makes zig
    build do better caching (try multiple zig builds in a row after this
    patch), as well as be set up for doing a system install or creating a
    distribution, with `zig build install --prefix /your/install/path`.
    
    A default `zig build` will "install" to `zig-cache`, so for typical
    development purposes you would run `./zig-cache/bin/zootdeck`.
    
    The formatting changes are due to having `zig fmt` enabled on save.
    
    Hash maps are upgraded to `std.StringHashMap` to fix compilation errors.
    
    Tested with zig 53ae03ebe9e0aa56bca1c9219a47d124b0435258.

--- src/config.zig
+++ src/config.zig
@@ -11,194 +11,197 @@ const toot_list = @import("./toot_list.zig");
 var allocator: *Allocator = undefined;
 
 const c = @cImport({
-  @cInclude("time.h");
+    @cInclude("time.h");
 });
 
 pub const Time = c.time_t;
 
 // contains runtime-only values
 pub const Settings = struct {
-  win_x: i64,
-  win_y: i64,
-  columns: std.ArrayList(*ColumnInfo),
-  config_path: []const u8,
+    win_x: i64,
+    win_y: i64,
+    columns: std.ArrayList(*ColumnInfo),
+    config_path: []const u8,
 };
 
 // on-disk format
 pub const ConfigFile = struct {
-  win_x: i64,
-  win_y: i64,
-  columns: std.ArrayList(*ColumnConfig)
+    win_x: i64,
+    win_y: i64,
+    columns: std.ArrayList(*ColumnConfig),
 };
 
 pub const ColumnInfo = struct {
-  config: *ColumnConfig,
-  filter: *filter_lib.ptree,
-  toots: toot_list.TootList,
-  refreshing: bool,
-  last_check: Time,
-  inError: bool,
-  account: ?std.hash_map.HashMap([]const u8,std.json.Value,std.mem.hash_slice_u8,std.mem.eql_slice_u8),
-  oauthClientId: ?[]const u8,
-  oauthClientSecret: ?[]const u8,
+    config: *ColumnConfig,
+    filter: *filter_lib.ptree,
+    toots: toot_list.TootList,
+    refreshing: bool,
+    last_check: Time,
+    inError: bool,
+    account: ?std.StringHashMap(std.json.Value),
+    oauthClientId: ?[]const u8,
+    oauthClientSecret: ?[]const u8,
 
-  const Self = @This();
+    const Self = @This();
 
-  pub fn reset(self: *const Self) void {
-  }
+    pub fn reset(self: *const Self) void {}
 
-  pub fn parseFilter(self: *const Self, filter: []const u8) void {
-    self.filter = filter_lib.parse(filter);
-  }
-
-  pub fn makeTitle(column: *ColumnInfo) []const u8 {
-    var out: []const u8 = column.filter.host();
-    if(column.config.token) |tkn| {
-      var addon: []const u8 = undefined;
-      if(column.account) |account| {
-        addon = account.get("acct").?.value.String;
-      } else {
-        addon = "_";
-      }
-      out = std.fmt.allocPrint(allocator, "{}@{}", addon, column.filter.host()) catch unreachable;
+    pub fn parseFilter(self: *const Self, filter: []const u8) void {
+        self.filter = filter_lib.parse(filter);
     }
-    return out;
-  }
+
+    pub fn makeTitle(column: *ColumnInfo) []const u8 {
+        var out: []const u8 = column.filter.host();
+        if (column.config.token) |tkn| {
+            var addon: []const u8 = undefined;
+            if (column.account) |account| {
+                addon = account.get("acct").?.value.String;
+            } else {
+                addon = "_";
+            }
+            out = std.fmt.allocPrint(allocator, "{}@{}", addon, column.filter.host()) catch unreachable;
+        }
+        return out;
+    }
 };
 
 pub const ColumnConfig = struct {
-  title: []const u8,
-  filter: []const u8,
-  token: ?[]const u8,
-  img_only: bool,
+    title: []const u8,
+    filter: []const u8,
+    token: ?[]const u8,
+    img_only: bool,
 
-  const Self = @This();
+    const Self = @This();
 };
 
 pub const LoginInfo = struct {
-  username: []const u8,
-  password: []const u8
+    username: []const u8,
+    password: []const u8,
 };
 
 pub const HttpInfo = struct {
-  url: []const u8,
-  verb: enum { get, post},
-  token: ?[]const u8,
-  post_body: []const u8,
-  body: []const u8,
-  content_type: []const u8,
-  response_code: c_long,
-  tree: std.json.ValueTree,
-  column: *ColumnInfo,
-  toot: *toot_lib.Type
+    url: []const u8,
+    verb: enum {
+        get,
+        post,
+    },
+    token: ?[]const u8,
+    post_body: []const u8,
+    body: []const u8,
+    content_type: []const u8,
+    response_code: c_long,
+    tree: std.json.ValueTree,
+    column: *ColumnInfo,
+    toot: *toot_lib.Type,
 };
 
 pub const ColumnAuth = struct {
-  code: []const u8,
-  column: *ColumnInfo
+    code: []const u8,
+    column: *ColumnInfo,
 };
 
 const ConfigError = error{MissingParams};
 
 pub fn init(alloc: *Allocator) !void {
-  allocator = alloc;
-  var trickZig = false;
-  if (trickZig) { return error.BadValue; }
+    allocator = alloc;
+    var trickZig = false;
+    if (trickZig) {
+        return error.BadValue;
+    }
 }
 
 pub fn readfile(filename: []const u8) !Settings {
-  if (std.fs.File.openWriteNoClobber(filename, std.fs.File.default_mode)) |*file| {
-    try file.write("{}\n");
-    warn("Warning: creating new {}\n", filename);
-    file.close();
-  } else |err| {} // existing file is OK
+    if (std.fs.File.openWriteNoClobber(filename, std.fs.File.default_mode)) |*file| {
+        try file.write("{}\n");
+        warn("Warning: creating new {}\n", filename);
+        file.close();
+    } else |err| {} // existing file is OK
 
-  var json = try std.io.readFileAlloc(allocator, filename);
-  return read(json);
+    var json = try std.io.readFileAlloc(allocator, filename);
+    return read(json);
 }
 
 pub fn read(json: []const u8) !Settings {
-  var json_parser = std.json.Parser.init(allocator, false);
-  var value_tree = try json_parser.parse(json);
-  var settings = allocator.create(Settings) catch unreachable;
-  var root = value_tree.root.Object;
-  settings.columns = std.ArrayList(*ColumnInfo).init(allocator);
+    var json_parser = std.json.Parser.init(allocator, false);
+    var value_tree = try json_parser.parse(json);
+    var settings = allocator.create(Settings) catch unreachable;
+    var root = value_tree.root.Object;
+    settings.columns = std.ArrayList(*ColumnInfo).init(allocator);
 
-  if (root.get("win_x")) |w| {
-    settings.win_x = w.value.Integer;
-  } else {
-    settings.win_x = 800;
-  }
-  if (root.get("win_y")) |h| {
-    settings.win_y = h.value.Integer;
-  } else {
-    settings.win_y = 600;
-  }
-  if (root.get("columns")) |columns| {
-    for(columns.value.Array.toSlice()) |value| {
-      var colInfo = allocator.create(ColumnInfo) catch unreachable;
-      colInfo.reset();
-      colInfo.toots = toot_list.TootList.init();
-      var colconfig = allocator.create(ColumnConfig) catch unreachable;
-      colInfo.config = colconfig;
-      var title = value.Object.get("title").?.value.String;
-      colInfo.config.title = title;
-      var filter = value.Object.get("filter").?.value.String;
-      colInfo.config.filter = filter;
-      colInfo.filter = filter_lib.parse(allocator, filter);
-      var tokenTag = value.Object.get("token");
-      if(tokenTag) |tokenKV| {
-        if(@TagType(std.json.Value)(tokenKV.value) == .String) {
-          colInfo.config.token = tokenKV.value.String;
-        } else {
-          colInfo.config.token = null;
-        }
-      } else {
-        colInfo.config.token = null;
-      }
-      var img_only = value.Object.get("img_only").?.value.Bool;
-      colInfo.config.img_only = img_only;
-      settings.columns.append(colInfo) catch unreachable;
+    if (root.get("win_x")) |w| {
+        settings.win_x = w.value.Integer;
+    } else {
+        settings.win_x = 800;
     }
-  } else {
-    warn("missing columns\n");
-  }
-  return settings.*;
+    if (root.get("win_y")) |h| {
+        settings.win_y = h.value.Integer;
+    } else {
+        settings.win_y = 600;
+    }
+    if (root.get("columns")) |columns| {
+        for (columns.value.Array.toSlice()) |value| {
+            var colInfo = allocator.create(ColumnInfo) catch unreachable;
+            colInfo.reset();
+            colInfo.toots = toot_list.TootList.init();
+            var colconfig = allocator.create(ColumnConfig) catch unreachable;
+            colInfo.config = colconfig;
+            var title = value.Object.get("title").?.value.String;
+            colInfo.config.title = title;
+            var filter = value.Object.get("filter").?.value.String;
+            colInfo.config.filter = filter;
+            colInfo.filter = filter_lib.parse(allocator, filter);
+            var tokenTag = value.Object.get("token");
+            if (tokenTag) |tokenKV| {
+                if (@TagType(std.json.Value)(tokenKV.value) == .String) {
+                    colInfo.config.token = tokenKV.value.String;
+                } else {
+                    colInfo.config.token = null;
+                }
+            } else {
+                colInfo.config.token = null;
+            }
+            var img_only = value.Object.get("img_only").?.value.Bool;
+            colInfo.config.img_only = img_only;
+            settings.columns.append(colInfo) catch unreachable;
+        }
+    } else {
+        warn("missing columns\n");
+    }
+    return settings.*;
 }
 
 pub fn writefile(settings: Settings, filename: []const u8) void {
-  var configFile = allocator.create(ConfigFile) catch unreachable;
-  configFile.win_x = @intCast(c_int, settings.win_x);
-  configFile.win_y = @intCast(c_int, settings.win_y);
-  configFile.columns = std.ArrayList(*ColumnConfig).init(allocator);
-  for(settings.columns.toSlice()) |column, idx| {
-    configFile.columns.append(column.config) catch unreachable;
-  }
-  if (std.fs.File.openWrite(filename)) |*file| {
-    warn("config.write toJson\n");
-    var data = Json.toJson(allocator, configFile);
-    file.write(data) catch unreachable;
-    warn("config saved. {} {} bytes\n", filename, data.len);
-    file.close();
-  } else |err| {
-    warn("config save fail. {}\n", err);
-  } // existing file is OK
+    var configFile = allocator.create(ConfigFile) catch unreachable;
+    configFile.win_x = @intCast(c_int, settings.win_x);
+    configFile.win_y = @intCast(c_int, settings.win_y);
+    configFile.columns = std.ArrayList(*ColumnConfig).init(allocator);
+    for (settings.columns.toSlice()) |column, idx| {
+        configFile.columns.append(column.config) catch unreachable;
+    }
+    if (std.fs.File.openWrite(filename)) |*file| {
+        warn("config.write toJson\n");
+        var data = Json.toJson(allocator, configFile);
+        file.write(data) catch unreachable;
+        warn("config saved. {} {} bytes\n", filename, data.len);
+        file.close();
+    } else |err| {
+        warn("config save fail. {}\n", err);
+    } // existing file is OK
 }
 
 pub fn now() Time {
-  var t: Time = undefined;
-  _ = c.time(&t);
-  return t;
+    var t: Time = undefined;
+    _ = c.time(&t);
+    return t;
 }
 
 const assert = @import("std").debug.assert;
 test "read" {
-  var ret = read("{\"url\":\"abc\"}");
-  if (ret) |value| {
-    assert(true);
-  } else |err| {
-    warn("warn: {}\n", err);
-    assert(false);
-  }
+    var ret = read("{\"url\":\"abc\"}");
+    if (ret) |value| {
+        assert(true);
+    } else |err| {
+        warn("warn: {}\n", err);
+        assert(false);
+    }
 }
-
--- src/toot.zig
+++ src/toot.zig
@@ -10,105 +10,102 @@ const json_lib = @import("json.zig");
 pub const Type = Toot();
 
 pub fn Toot() type {
-  return struct {
-    hashmap: Toothashmap,
-    tagList: TagList,
-    imgList: ImgList,
+    return struct {
+        hashmap: Toothashmap,
+        tagList: TagList,
+        imgList: ImgList,
 
-    const Self = @This();
-    const TagType = []const u8;
-    pub const TagList = std.ArrayList(TagType);
-    const ImgType = []const u8;
-    const ImgList = std.ArrayList(ImgType);
-    const K = []const u8;
-    const V = std.json.Value;
-    const Toothashmap = std.hash_map.HashMap(K, V,
-                                             std.mem.hash_slice_u8,
-                                             std.mem.eql_slice_u8);
-    pub fn init(hash: Toothashmap, allocator: *Allocator) Self {
-     var newToot = Self{
-        .hashmap = hash,
-        .tagList = TagList.init(allocator),
-        .imgList = ImgList.init(allocator)
-      };
-      newToot.parseTags(allocator);
-      return newToot;
-    }
-
-    pub fn get(self: *const Self, key: K) ?*Toothashmap.KV {
-      return self.hashmap.get(key);
-    }
-
-    pub fn id(self: *const Self) []const u8 {
-      if(self.hashmap.get("id")) |kv| {
-        return kv.value.String;
-      } else {
-        unreachable;
-      }
-    }
-
-    pub fn author(self: *const Self, acct: []const u8) bool {
-      if(self.hashmap.get("account")) |kv| {
-        if(kv.value.Object.get("acct")) |akv| {
-          const existing_acct = akv.value.String;
-          return std.mem.compare(u8, acct, existing_acct) == std.mem.Compare.Equal;
-        } else {
-          return false;
+        const Self = @This();
+        const TagType = []const u8;
+        pub const TagList = std.ArrayList(TagType);
+        const ImgType = []const u8;
+        const ImgList = std.ArrayList(ImgType);
+        const K = []const u8;
+        const V = std.json.Value;
+        const Toothashmap = std.StringHashMap(V);
+        pub fn init(hash: Toothashmap, allocator: *Allocator) Self {
+            var newToot = Self{
+                .hashmap = hash,
+                .tagList = TagList.init(allocator),
+                .imgList = ImgList.init(allocator),
+            };
+            newToot.parseTags(allocator);
+            return newToot;
         }
-      } else {
-        return false;
-      }
-    }
 
-    pub fn content(self: *const Self) []const u8 {
-      return self.hashmap.get("content").?.value.String;
-    }
-
-    pub fn parseTags(self: *Self, allocator: *Allocator) void {
-      const content_zig_namespace_yuk = self.content();
-      const jDecode = json_lib.jsonStrDecode(content_zig_namespace_yuk, allocator)  catch unreachable;
-      const hDecode = util.htmlEntityDecode(jDecode, allocator)  catch unreachable;
-      const html_trim = util.htmlTagStrip(hDecode, allocator) catch unreachable;
-
-      var wordParts = std.mem.tokenize(html_trim, " ");
-      while (wordParts.next()) |word| {
-        if(std.mem.startsWith(u8, word, "#")) {
-          self.tagList.append(word) catch unreachable;
+        pub fn get(self: *const Self, key: K) ?*Toothashmap.KV {
+            return self.hashmap.get(key);
         }
-      }
-    }
 
-    pub fn addImg(self: *Self, imgdata: ImgType) void {
-      warn("addImg toot {*}\n", self);
-      self.imgList.append(imgdata) catch unreachable;
-    }
+        pub fn id(self: *const Self) []const u8 {
+            if (self.hashmap.get("id")) |kv| {
+                return kv.value.String;
+            } else {
+                unreachable;
+            }
+        }
 
-    pub fn imgCount(self: *Self) usize {
-      var images = self.hashmap.get("media_attachments").?.value.Array;
-      return images.len;
-    }
-  };
+        pub fn author(self: *const Self, acct: []const u8) bool {
+            if (self.hashmap.get("account")) |kv| {
+                if (kv.value.Object.get("acct")) |akv| {
+                    const existing_acct = akv.value.String;
+                    return std.mem.compare(u8, acct, existing_acct) == std.mem.Compare.Equal;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        pub fn content(self: *const Self) []const u8 {
+            return self.hashmap.get("content").?.value.String;
+        }
+
+        pub fn parseTags(self: *Self, allocator: *Allocator) void {
+            const content_zig_namespace_yuk = self.content();
+            const jDecode = json_lib.jsonStrDecode(content_zig_namespace_yuk, allocator) catch unreachable;
+            const hDecode = util.htmlEntityDecode(jDecode, allocator) catch unreachable;
+            const html_trim = util.htmlTagStrip(hDecode, allocator) catch unreachable;
+
+            var wordParts = std.mem.tokenize(html_trim, " ");
+            while (wordParts.next()) |word| {
+                if (std.mem.startsWith(u8, word, "#")) {
+                    self.tagList.append(word) catch unreachable;
+                }
+            }
+        }
+
+        pub fn addImg(self: *Self, imgdata: ImgType) void {
+            warn("addImg toot {*}\n", self);
+            self.imgList.append(imgdata) catch unreachable;
+        }
+
+        pub fn imgCount(self: *Self) usize {
+            var images = self.hashmap.get("media_attachments").?.value.Array;
+            return images.len;
+        }
+    };
 }
 
 test "Toot" {
-  var bytes: [8096]u8 = undefined;
-  const allocator = &std.heap.FixedBufferAllocator.init(bytes[0..]).allocator;
-  var tootHash = Type.Toothashmap.init(allocator);
+    var bytes: [8096]u8 = undefined;
+    const allocator = &std.heap.FixedBufferAllocator.init(bytes[0..]).allocator;
+    var tootHash = Type.Toothashmap.init(allocator);
 
-  var jString = std.json.Value{ .String = "" };
-  _ = tootHash.put("content", jString) catch unreachable;
+    var jString = std.json.Value{ .String = "" };
+    _ = tootHash.put("content", jString) catch unreachable;
 
-  jString.String = "ABC";
-  _ = tootHash.put("content", jString) catch unreachable;
-  var toot = Type.init(tootHash, allocator);
-  testing.expect(toot.tagList.count() == 0);
-  warn("toot1 {*}\n", &toot);
+    jString.String = "ABC";
+    _ = tootHash.put("content", jString) catch unreachable;
+    var toot = Type.init(tootHash, allocator);
+    testing.expect(toot.tagList.count() == 0);
+    warn("toot1 {*}\n", &toot);
 
-  jString.String = "ABC   #xyz";
-  _ = tootHash.put("content", jString) catch unreachable;
-  const toot2 = Type.init(tootHash, allocator);
-  warn("toot2 {*}\n", &toot2);
-  testing.expect(toot2.tagList.count() == 1);
-  testing.expect(std.mem.compare(u8, toot2.tagList.at(0), "#xyz") == std.mem.Compare.Equal);
+    jString.String = "ABC   #xyz";
+    _ = tootHash.put("content", jString) catch unreachable;
+    const toot2 = Type.init(tootHash, allocator);
+    warn("toot2 {*}\n", &toot2);
+    testing.expect(toot2.tagList.count() == 1);
+    testing.expect(std.mem.compare(u8, toot2.tagList.at(0), "#xyz") == std.mem.Compare.Equal);
 }
-
