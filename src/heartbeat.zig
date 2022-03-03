ff5a9000458bfb2c0e1e179c0bbfd0deeb375be6
dp <donp@donp.org>
2022-03-03 08:23:43 -0800

    slow down the heartbeat

--- src/heartbeat.zig
+++ src/heartbeat.zig
@@ -12,12 +12,13 @@ pub fn init(myAllocator: Allocator) !void {
     allocator = myAllocator;
 }
 
-pub fn go(data: ?*anyopaque) callconv(.C) ?*anyopaque {
-    var data8 = @alignCast(@alignOf(thread.Actor), data);
+pub fn go(actor_ptr: ?*anyopaque) callconv(.C) ?*anyopaque {
+    var data8 = @alignCast(@alignOf(thread.Actor), actor_ptr);
     var actor = @ptrCast(*thread.Actor, data8);
+    const seconds = 60;
     warn("heartbeat mainloop thread.self()={} actor.thread_id={} \n", .{ thread.self(), actor.thread_id });
     while (true) {
-        _ = c.usleep(3 * 1000000);
+        _ = c.usleep(seconds * 1000000);
         // signal crazy
         var command = allocator.create(thread.Command) catch unreachable;
         var verb = allocator.create(thread.CommandVerb) catch unreachable;
